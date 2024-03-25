4932e6739526da510ff6f47d392c3c96f3faf5b2
Zafran Khan <zafrankhattak480@gmail.com>
2024-03-25 23:23:38 +0500

    Change Route

--- src/app/HomePage/page.tsx
+++ src/app/HomePage/page.tsx
@@ -1,11 +1,15 @@
+'use client'
 import Link from "next/link";
 
+import { useRouter } from 'next/navigation';
 function HomePage() {
+  const route = useRouter();
   return (
     <div>
     <div>This is a HomePage</div>
 
     <Link href={"/NamePage"}>Click Me</Link>
+    <p onClick={()=>route.push('/NamePage')}>Click me</p>
     </div>
   )
 }
